d070935bd0e667cfdd72d8aa3c81967008f124c3
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-01-30 16:37:03 +0100

    HeadOffice entity

---
+++ src/main/java/cabanas/garcia/ismael/management/domain/model/HeadOffice.java
@@ -0,0 +1,21 @@
+package cabanas.garcia.ismael.management.domain.model;
+
+import cabanas.garcia.ismael.snackmachine.domain.model.AgreggateRoot;
+
+public class HeadOffice extends AgreggateRoot<HeadOfficeId>{
+
+    private double balance;
+
+    public HeadOffice() {
+        super(new HeadOfficeId());
+        this.balance = 0;
+    }
+
+    public void changeBalance(double amount) {
+        this.balance += amount;
+    }
+
+    public double balance() {
+        return balance;
+    }
+}
