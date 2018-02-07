2d70d5f27da9f84519944f44e1a548cda34889a7
Ismael Cabañas García <ismaelcabanas@gmail.com>
2018-02-07 07:54:54 +0100

    Injection dependencies for Event Processor

---
+++ src/main/java/cabanas/garcia/ismael/dddinpractice/atm/infrastructure/framework/configuration/EventProcessorConfiguration.java
@@ -0,0 +1,28 @@
+package cabanas.garcia.ismael.dddinpractice.atm.infrastructure.framework.configuration;
+
+import cabanas.garcia.ismael.dddinpractice.management.domain.model.BalanceChargedHandler;
+import cabanas.garcia.ismael.dddinpractice.management.domain.repository.HeadOfficeRepository;
+import cabanas.garcia.ismael.dddinpractice.shared.domain.service.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class EventProcessorConfiguration {
+    @Bean
+    public List<Handler> handlers(HeadOfficeRepository headOfficeRepository) {
+        return Arrays.asList(new BalanceChargedHandler(headOfficeRepository));
+    }
+
+    @Bean
+    public EventDispatcher eventDispatcher(List<Handler> handlers) {
+        return new DefaultEventDispatcher(handlers);
+    }
+
+    @Bean
+    public EventProcessor eventProcessor(EventDispatcher eventDispatcher) {
+        return new DefaultEventProcessor(eventDispatcher);
+    }
+}
